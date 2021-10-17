88fde9e7deddd69ca4ed0522fea4193c2ef25421
Chandra <jjchandru@yahoo.com>
2021-10-17 12:41:08 +0530

    initial version

---
+++ galaxy-store-rest/src/main/java/com/revature/galaxystore/GalaxyStoreRestApplication.java
@@ -0,0 +1,13 @@
+package com.revature.galaxystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyStoreRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyStoreRestApplication.class, args);
+	}
+
+}
---
+++ galaxy-store-rest/src/test/java/com/revature/galaxystore/GalaxyStoreRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.galaxystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaxyStoreRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
